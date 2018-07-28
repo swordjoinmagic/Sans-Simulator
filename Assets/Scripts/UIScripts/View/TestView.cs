33dd1099b762d2c922c4cf18cdd44bbc9079fa0e
swordjoinmagic <2483787753@qq.com>
2018-07-28 21:45:35 +0800

    init

---
+++ Assets/Scripts/UIScripts/View/TestView.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using uMVVM;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.UIScripts.View {
+    class TestView : UnityGuiView<TestViewModel>{
+
+        //===========================================
+        //此View控制的UI控件
+        public Text text;
+
+        public void Awake() {
+            BindingContext = new TestViewModel();
+
+            // 显示该View
+            Reveal();
+        }
+
+        protected override void OnInitialize() {
+            base.OnInitialize();
+            binder.Add<string>("text", OnTextValueChanged);
+        }
+
+
+        // 数据绑定事件
+        private void OnTextValueChanged(string oldValue,string newValue) {
+            text.text = newValue;
+        }
+
+    }
+}
---
+++ Assets/Scripts/UIScripts/ViewModel/TestViewModel.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using uMVVM;
+
+public class TestViewModel : ViewModelBase{
+    public BindableProperty<string> text = new BindableProperty<string>();
+}
